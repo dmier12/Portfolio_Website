222965799fecc708bf1438e79d4fe97770605736
dmier12 <113485490+dmier12@users.noreply.github.com>
2023-07-14 19:20:37 -0700

    Portfolio v0.1
    Adding what I have done so far

---
+++ Script1.js
@@ -0,0 +1,7 @@
+// Example: Display a popup when a button is clicked
+const button = document.querySelector('#contact button');
+button.addEventListener('click', () => {
+    alert('Thank you for contacting me!');
+});
+
+
